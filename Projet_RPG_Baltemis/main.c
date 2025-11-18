417f383851eb7634b5f208162a3843d75fdff068
BaldisDafort <samuel.doidi@gmail.com>
2025-11-18 10:08:42 +0100

    Init window and event

--- Projet_RPG_Baltemis/main.c
+++ Projet_RPG_Baltemis/main.c
@@ -2,5 +2,23 @@
 
 int main()
 {
+	sfVideoMode mode = { 800,600,32 };
+	sfRenderWindow* window = sfRenderWindow_create(mode, "CSFML", sfResize | sfClose, NULL);
 
+	sfEvent event;
+
+	while (sfRenderWindow_isOpen(window)) // Boucle de jeu
+	{
+		// Boucle de gestion des events
+		while (sfRenderWindow_pollEvent(window, &event))
+		{
+			if (event.type == sfEvtClosed) //Gestion de la croix de la barre de titre
+			{
+				sfRenderWindow_close(window); // Ferme la fenêtre et coupe la boucle de jeu
+			}
+		}
+		sfRenderWindow_clear(window, sfBlue); 
+
+		sfRenderWindow_display(window); // Affiche tout ce qui a été dessiné entre le clear et cette ligne
+	}
 }
