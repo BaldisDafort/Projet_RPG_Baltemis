d429fcca93203cab2d89f04fc869a53ab16d0608
Artemis30-7 <angelysbousquet@gmail.com>
2025-11-18 13:23:38 +0100

    windows view created

--- Projet_RPG_Baltemis/view.c
+++ Projet_RPG_Baltemis/view.c
@@ -1,9 +1,13 @@
 #include "view.h"
 
 //view map dezoome
-sfView* viewEditor;
 sfVector2f posViewEditor = { mapsizeX * tilesize / 2 + 1.5 * tilesize, mapsizeY * tilesize / 2 };
 sfFloatRect rectViewEditor = { mapsizeX * tilesize / 2 + 1.5 * tilesize, mapsizeY * tilesize / 2, mapsizeX * tilesize, mapsizeY * tilesize };
+
+//view tuiles a selectionner
+sfVector2f posView = { 400, 300 };
+sfFloatRect rectView = { 400, 300, 800, 600 };
+
 
 void initView()
 {
@@ -11,6 +15,9 @@ void initView()
 	viewEditor = sfView_create();
 	sfView_reset(viewEditor, rectViewEditor);
 
+	//view tuiles a selectionner
+	view = sfView_create();
+	sfView_reset(view, rectView);
 }
 
 void updateView(sfRenderWindow* _window)
@@ -19,10 +26,18 @@ void updateView(sfRenderWindow* _window)
 	sfView_setCenter(viewEditor, posViewEditor);
 	sfView_setSize(viewEditor, (sfVector2f) { mapsizeX* tilesize + 3 * tilesize, mapsizeY* tilesize + 3 * tilesize });
 
+	//view map dezoome
+	sfView_setCenter(view, posView);
 }
 
 //view map dezoome
 void displayView(sfRenderWindow* _window)
 {
 	sfRenderWindow_setView(_window, viewEditor);
+}
+
+void displayEditorView(sfRenderWindow* _window)
+{
+	sfRenderWindow_setView(_window, view);
+
 }
