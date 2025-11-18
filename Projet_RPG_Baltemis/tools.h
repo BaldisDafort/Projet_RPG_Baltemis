1d80a79d379f4c26fe9aae4f01bc9f3e29aa498e
Artemis30-7 <angelysbousquet@gmail.com>
2025-11-18 09:11:37 +0100

    update tools.h

--- Projet_RPG_Baltemis/tools.h
+++ Projet_RPG_Baltemis/tools.h
@@ -1,2 +1,14 @@
-#include "stdlib.h"
-#include "stdio.h"
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <Windows.h>
+#include "SFML/Graphics.h"
+#include "SFML/Audio.h"
+#include <time.h>
+
+void initTools();
+void restartClock();
+float GetDeltaTime();
+
