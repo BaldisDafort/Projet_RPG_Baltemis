6d17a810880f65211f76346f8e2dc83fdfb0afb1
BaldisDafort <samuel.doidi@gmail.com>
2025-11-19 13:41:21 +0100

    Add buttons in display Option

--- Projet_RPG_Baltemis/options.h
+++ Projet_RPG_Baltemis/options.h
@@ -3,6 +3,8 @@
 #include "tools.h"
 
 extern sfRectangleShape* g_DisplayOptions;
+extern sfRectangleShape* g_GeneralSoundButton_IsMuted;
+extern sfRectangleShape* g_SFXSoundButton_IsMuted;
 
 void initOptions();
 void displayOptions(sfRenderWindow* _window);
