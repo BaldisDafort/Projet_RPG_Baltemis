8d79b491a98085c0ceb00b591250812a10d52294
BaldisDafort <samuel.doidi@gmail.com>
2025-11-20 10:16:29 +0100

    add sprite button

--- Projet_RPG_Baltemis/title_screen.c
+++ Projet_RPG_Baltemis/title_screen.c
@@ -8,8 +8,16 @@ sfSprite* spriteStartButton;
 sfIntRect startButtonAnimationRect = { 0,0,100,35 };
 
 sfRectangleShape* EditorButton;
+sfSprite* spriteEditorButton;
+sfIntRect editorButtonAnimationRect = { 0,0,100,35 };
+
 sfRectangleShape* OptionsButton;
+sfSprite* spriteOptionsButton;
+sfIntRect optionsButtonAnimationRect = { 0,0,100,35 };
+
 sfRectangleShape* QuitButton;
+sfSprite* spriteQuitButton;
+sfIntRect quitButtonAnimationRect = { 0,0,100,35 };
 
 void initTitleScreen()
 {
@@ -17,14 +25,12 @@ void initTitleScreen()
 	sfTexture* tTitleScreen = sfTexture_createFromFile("..\\Resources\\Images\\titleScreen.png", NULL);
 	sfSprite_setTexture(sTitleScreen, tTitleScreen, sfTrue);
 
-
-
-	//sfVector2f scale = { 2.f,2.65f };
-	//sfSprite_scale(sTitleScreen, scale);
-
+	// Start Button
 	StartButton = sfRectangleShape_create();
 	sfVector2f sizeStartButton = { 100.f, 35.f };
-	sfVector2f positionStartButton = { 375.f, 350.f };
+	sfVector2f positionStartButton = { 351.f, 350.f };
+	sfColor startButtonColor = sfColor_fromRGBA(255, 255, 255, 0);
+	sfRectangleShape_setFillColor(StartButton, startButtonColor);
 	sfRectangleShape_setSize(StartButton, sizeStartButton);
 	sfRectangleShape_setPosition(StartButton, positionStartButton);
 
@@ -34,42 +40,67 @@ void initTitleScreen()
 	sfSprite_setTextureRect(spriteStartButton, startButtonAnimationRect);
 	sfSprite_setPosition(spriteStartButton, positionStartButton);
 
+	// Editor Button
 	EditorButton = sfRectangleShape_create();
 	sfVector2f sizeEditorButton = { 100.f, 35.f };
-	sfVector2f positionEditorButton = { 375.f, 410.f };
+	sfVector2f positionEditorButton = { 351.f, 410.f };
+	sfColor editorButtonColor = sfColor_fromRGBA(255, 255, 255, 0);
+	sfRectangleShape_setFillColor(EditorButton, editorButtonColor);
 	sfRectangleShape_setSize(EditorButton, sizeEditorButton);
-	sfRectangleShape_setFillColor(EditorButton, sfGreen);
 	sfRectangleShape_setPosition(EditorButton, positionEditorButton);
-	sfRectangleShape_setOutlineThickness(EditorButton, 5.f);
-	sfRectangleShape_setOutlineColor(EditorButton, sfYellow);
 
+
+	spriteEditorButton = sfSprite_create();
+	sfTexture* textureEditButton = sfTexture_createFromFile("..\\Resources\\Textures\\edit-button.png", NULL);
+	sfSprite_setTexture(spriteEditorButton, textureEditButton, sfTrue);
+	sfSprite_setTextureRect(spriteEditorButton, editorButtonAnimationRect);
+	sfSprite_setPosition(spriteEditorButton, positionEditorButton);
+
+	// Option Button
 	OptionsButton = sfRectangleShape_create();
 	sfVector2f sizeOptionsButton = { 100.f, 35.f };
-	sfVector2f positionOptionsButton = { 375.f, 470.f };
-	sfColor OptionsButtonColor = sfColor_fromRGBA(70, 70, 70, 255);
+	sfVector2f positionOptionsButton = { 351.f, 470.f };
+	sfColor OptionsButtonColor = sfColor_fromRGBA(255, 255, 255, 0);
 	sfRectangleShape_setSize(OptionsButton, sizeOptionsButton);
 	sfRectangleShape_setFillColor(OptionsButton, OptionsButtonColor);
 	sfRectangleShape_setPosition(OptionsButton, positionOptionsButton);
-	sfRectangleShape_setOutlineThickness(OptionsButton, 5.f);
-	sfRectangleShape_setOutlineColor(OptionsButton, sfYellow);
 
+	spriteOptionsButton = sfSprite_create();
+	sfTexture* textureOptionsButton = sfTexture_createFromFile("..\\Resources\\Textures\\option.png", NULL);
+	sfSprite_setTexture(spriteOptionsButton, textureOptionsButton, sfTrue);
+	sfSprite_setTextureRect(spriteOptionsButton, optionsButtonAnimationRect);
+	sfSprite_setPosition(spriteOptionsButton, positionOptionsButton);
+
+	// Quit Button
 	QuitButton = sfRectangleShape_create();
 	sfVector2f sizeQuitButton = { 100.f, 35.f };
-	sfVector2f positionQuitButton = { 375.f, 530.f };
+	sfVector2f positionQuitButton = { 351.f, 530.f };
+	sfColor quitButtonColor = sfColor_fromRGBA(255, 255, 255, 0);
+	sfRectangleShape_setFillColor(QuitButton, quitButtonColor);
 	sfRectangleShape_setSize(QuitButton, sizeQuitButton);
-	sfRectangleShape_setFillColor(QuitButton, sfBlue);
 	sfRectangleShape_setPosition(QuitButton, positionQuitButton);
-	sfRectangleShape_setOutlineThickness(QuitButton, 5.f);
-	sfRectangleShape_setOutlineColor(QuitButton, sfYellow);
+
+	spriteQuitButton = sfSprite_create();
+	sfTexture* textureQuitButton = sfTexture_createFromFile("..\\Resources\\Textures\\exit-button.png", NULL);
+	sfSprite_setTexture(spriteQuitButton, textureQuitButton, sfTrue);
+	sfSprite_setTextureRect(spriteQuitButton, quitButtonAnimationRect);
+	sfSprite_setPosition(spriteQuitButton, positionQuitButton);
 
 }
 
 void displayTitleScreen(sfRenderWindow* _window)
 {
 	sfRenderWindow_drawSprite(_window, sTitleScreen, NULL);
+
 	sfRenderWindow_drawRectangleShape(_window, StartButton, NULL);
 	sfRenderWindow_drawSprite(_window, spriteStartButton, NULL);
+
 	sfRenderWindow_drawRectangleShape(_window, EditorButton, NULL);
+	sfRenderWindow_drawSprite(_window, spriteEditorButton, NULL);
+
 	sfRenderWindow_drawRectangleShape(_window, OptionsButton, NULL);
+	sfRenderWindow_drawSprite(_window, spriteOptionsButton, NULL);
+
 	sfRenderWindow_drawRectangleShape(_window, QuitButton, NULL);
+	sfRenderWindow_drawRectangleShape(_window, spriteQuitButton, NULL);
 }
