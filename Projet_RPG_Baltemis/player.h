a003f507a9c3e764d32575c9f007b429f06336c7
Artemis30-7 <angelysbousquet@gmail.com>
2025-11-20 12:25:28 +0100

    upload functional bat direction keyboard

--- Projet_RPG_Baltemis/player.h
+++ Projet_RPG_Baltemis/player.h
@@ -5,13 +5,30 @@ typedef enum Direction Direction;
 enum Direction
 {
 	BAS = 0,
+	HAUT,
 	DROITE,
-	GAUCHE,
-	HAUT
+	GAUCHE
 };
-int frameY;
+
+typedef struct BatPlayer
+{
+	sfSprite* spBat;
+	sfTexture* texBat;
+	sfFloatRect playerRect;
+	sfIntRect irectBat;
+	sfVector2f SpritePositionBat;
+	sfVector2f vitesse;
+	float animBatTime;
+	float timerBat;
+	float animBatSpeed;
+	int frameBatX;
+	int frameBatY;
+
+}BatPlayer;
+BatPlayer batPlayer;
+
 sfVector2f vitesse;
-sfBool isMoving;
+sfBool batIsMoving;
 
 void initPlayer();
 void updatePlayer();
