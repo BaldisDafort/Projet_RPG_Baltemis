eb8f4439151c8b82b35daf37a1ec03c4f2b4f1fe
Artemis30-7 <angelysbousquet@gmail.com>
2025-11-25 15:47:39 +0100

    update anims.c

--- Projet_RPG_Baltemis/anims.c
+++ Projet_RPG_Baltemis/anims.c
@@ -1,11 +1,32 @@
 #include "anims.h"
 
+void pike_isActived()
+{
+    float animPikeTimer = 0.0f;
+    if (animPikeTimer > 0.1f)
+    {
+        for (int i = 0; i < mapSizeY; i++)
+        {
+            for (int j = 0; j < mapSizeX; j++)
+            {
+                if (arr.mapObj[i][j].y == 6)
+                {
+                    arr.mapObj[i][j].x++;
+                    if (arr.mapObj[i][j].x > 7) arr.mapObj[i][j].x = 0;
+                }
+            }
+        }
+
+        animPikeTimer = 0.0f;
+    }
+
+}
+
+
 float animTimerCont;
 float animTimerCont_l;
 sfBool animLentePlaying;
 float cooldownLente;
-
-
 
 void initAnims()
 {
@@ -74,6 +95,10 @@ void updateAnims()
         animTimerCont_l = 0.0f;
     }
     cooldownLente += GetDeltaTime();
+
+
+
+    //anime des piege piquant
 }
 
 void displayAnims(sfRenderWindow* window)
